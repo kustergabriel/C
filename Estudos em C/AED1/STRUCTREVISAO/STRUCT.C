188493df07000c06b3eb19982107e2337637bb8e
Gabriel Azevedo <gabrielazevedo1812@gmail.com>
2024-07-25 20:53:13 -0300

    Update STRUCT.C

--- Estudos em C/AED1/STRUCTREVISAO/STRUCT.C
+++ Estudos em C/AED1/STRUCTREVISAO/STRUCT.C
@@ -16,26 +16,36 @@ typedef struct DadosEmpresa {
 
 void inserirnome(Empresa *emp);
 void insereridade(Empresa *emp);
+void inserirsexo(Empresa *emp);
 
 int main() {
     Empresa emp;
 
     inserirnome(&emp);
     insereridade(&emp);
+    inserirsexo (&emp);
     printf("Seu Nome: %s", emp.nome);
-    printf("Idade: %d", emp.idade);
+    printf("Idade: %d\n", emp.idade);
+    printf("Seu Sexo: %s", emp.sexo);
 
     return 0;
 }
 
 void inserirnome(Empresa *emp) {
     printf("Digite seu nome, por favor: ");
-    fgets(emp->nome, tam, stdin);
+    fgets(emp->nome, tam,stdin);
 
 }
 
 void insereridade(Empresa *emp) {
     printf("Insira sua idade: ");
     scanf("%d", &emp->idade);
+    while (getchar() != '\n');
+
+}
+
+void inserirsexo(Empresa *emp){
+    printf("Insira seu sexo: ");
+    fgets(emp->sexo, tam, stdin);
 
 }
